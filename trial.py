31ffd76c1eae75a2ad0b39286c23e531061b4055
Dev <davinderdevgrewal@gmail.com>
2024-09-02 15:43:07 +0100

    update

--- trial.py
+++ trial.py
@@ -9,9 +9,9 @@ def multiplication_or_sum(num1, num2):
         return num1 + num2
 
 # first condition
-result = multiplication_or_sum(20, 30)
+result = multiplication_or_sum(15, 50)
 print("The result is", result)
 
 # Second condition
-result = multiplication_or_sum(40, 30)
+result = multiplication_or_sum(10, 65)
 print("The result is", result)
